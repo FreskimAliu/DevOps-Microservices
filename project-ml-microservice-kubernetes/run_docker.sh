22f9f9cd0798249d55018021105f5c9719fee21a
FreskimAliu <freskimaliu@gmail.com>
2021-07-25 15:53:22 +0000

    Upload docker script to automate process of uploading images

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 # Build image and add a descriptive tag
+echo "Build docker image"
 docker build --tag=udacity-price-predictions .
 
 # List docker images
+echo "Listing docker images"
 docker image ls
 
 # Run flask app
+echo "Run flask app"
 docker run -p 8000:80 udacity-price-predictions
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
-# Assumes that an image is built via `run_docker.sh`
-
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+docker_id=freskimaliu
+tag=udacity-price-predictions
+dockerpath=$docker_id/$tag
 
-# Step 2:  
+
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username freskimaliu
+docker tag $tag $dockerpath
 
-# Step 3:
+
 # Push image to a docker repository
+docker push $dockerpath
