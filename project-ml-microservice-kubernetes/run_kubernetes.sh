463c550954f8d0edae80411c5937883a16c307d8
FreskimAliu <freskimaliu@gmail.com>
2021-07-25 18:17:06 +0000

    Update kubernetes script

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+docker_id=freskimaliu
+tag=udacity-price-predictions
+dockerpath=$docker_id/$tag
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment --image=$dockerpath $tag
 
-
-# Step 3:
 # List kubernetes pods
+pod=$(kubectl get pods | grep udacity-price-predictions | cut -d " " -f1)
+status=$(kubectl get pods $pod --no-headers -o custom-columns=":status.phase");
+while [ $status != 'Running' ]
+do
+  sleep 10;
+  status=$(kubectl get pods $pod --no-headers -o custom-columns=":status.phase");
+done
+kubectl get pods 
 
-# Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/$tag 8000:80
